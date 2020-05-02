d4871ed0f78ae826b93e7b7a8c902870e8b3e861
cf <abtjh5q@hmamail.com>
2020-05-02 11:01:46 +0200

    Add precompiled header for project
    Forgot to add this header when enabling PCH.

---
+++ Include/PCH.h
@@ -0,0 +1,16 @@
+/*
+ * PCH.h
+ *
+ *  Created on: May 1, 2020
+ *      Author: cf
+ */
+
+#pragma once
+
+#include <array>
+#include <functional>
+#include <memory>
+#include <type_traits>
+#include <vector>
+
+#include "fuchsch/Utilities/Observer.h"
